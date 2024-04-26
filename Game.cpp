36575203384c2055b0c6e0b3794566afb6c46fe0
lingxuanbing123 <llt2lxy@163.com>
2024-04-26 09:14:03 +0800

    Bullet.cpp

---
+++ Game.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdlib.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <conio.h>
+#include <time.h>
+
+#include <graphics.h>
+
+// esayx
+#include "Game.h"
+#include "Prop.h"
+#include "Plane.h"
+#include "Shoot.h"
+#include "BulletEnemy.h"
+#include "Boom.h"
+#include "Bullet.h"
+#include "Interface.h"
+#include "PlaneEnemy.h"
+#include "Operate.h"
+#include "Score.h"
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
